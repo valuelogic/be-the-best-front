dec79e74003704135c8c3a11f265b59ad7adcce7
Maciej Czekaj <mczekaj@valuelogic.one>
2022-07-21 12:42:55 +0200

    Change run settings for Jetbrains IDEs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { MoralisProvider } from "react-moralis";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { MoralisProvider } from 'react-moralis';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <MoralisProvider initializeOnMount={false}>
-      <Component {...pageProps} />
-    </MoralisProvider>
-  );
+	return (
+		<MoralisProvider initializeOnMount={false}>
+			<Component {...pageProps} />
+		</MoralisProvider>
+	);
 }
 
 export default MyApp;
